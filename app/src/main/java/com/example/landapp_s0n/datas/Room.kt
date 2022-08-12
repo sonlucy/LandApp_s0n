4c43344db69e77f74d34a4c17a84e913b5965e02
s0n <shson971029@naver.com>
2022-08-12 20:41:25 +0900

    방의 기능으로 -> 층수를 가공해서 String으로 주는 기능 추가

--- app/src/main/java/com/example/landapp_s0n/datas/Room.kt
+++ app/src/main/java/com/example/landapp_s0n/datas/Room.kt
@@ -6,6 +6,23 @@ class Room(
     val floor: Int,
     val description: String) {
 
+    fun getFormattedFloor() :String {
+
+        if (this.floor>=1)
+        {
+            return "${this.floor}층"
+        }
+        else if (this.floor==0)
+        {
+            return "반지하"
+        }
+        else
+        {
+            return "지하 ${-this.floor}층"
+        }
+
+
+    }
 
 
 }
