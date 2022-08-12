4d1112ecf80b93e83382d7fc24aee270d1d363d0
s0n <shson971029@naver.com>
2022-08-12 21:31:38 +0900

    방 상세정보로 통째로 데이터 넘겨서 반영

--- app/src/main/java/com/example/landapp_s0n/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/example/landapp_s0n/ViewRoomDetailActivity.kt
@@ -2,10 +2,24 @@ package com.example.landapp_s0n
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.landapp_s0n.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+// / 데이터 받기
+        /////Serializable을 room형태로 변환하기위해 casting해주기 (as사용)
+        val roomData = intent.getSerializableExtra("room") as Room
+
+        priceTxt.text = roomData.getFormattedPrice()
+        descriptionTxt.text = roomData.description
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFormattedFloor()
+
+
     }
 }
