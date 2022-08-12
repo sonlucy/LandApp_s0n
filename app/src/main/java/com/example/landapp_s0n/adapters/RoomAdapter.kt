431397178c50b2989b4edf60d5c85a95a041792d
s0n <shson971029@naver.com>
2022-08-12 20:43:31 +0900

    만들어낸 방의 층수 가공 기능 활용

--- app/src/main/java/com/example/landapp_s0n/adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/landapp_s0n/adapters/RoomAdapter.kt
@@ -31,7 +31,7 @@ class RoomAdapter(
         val descriptionTxt =row.findViewById<TextView>(R.id.descriptionTxt)
 
         descriptionTxt.text=roomData.description
-        addressAndFloorTxt.text=roomData.address
+        addressAndFloorTxt.text= "${roomData.address}, ${roomData.getFormattedFloor()}"
 
         return row
 
