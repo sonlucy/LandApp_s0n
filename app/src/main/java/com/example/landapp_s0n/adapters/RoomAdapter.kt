fa1a987fc4e99104d1e9f8c8dcde1205d582819e
s0n <shson971029@naver.com>
2022-08-12 20:17:46 +0900

    방 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/example/landapp_s0n/adapters/RoomAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.landapp_s0n.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.landapp_s0n.R
+import com.example.landapp_s0n.datas.Room
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {  //Room데이터를 뿌려주기위한 어댑터
+
+        val inflater = LayoutInflater.from(mContext)  //mContext화면을 기반으로 인플레이터 만들래.
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow =convertView
+        if (tempRow==null){
+            tempRow=inflater.inflate(R.layout.room_list_item, null)
+        }
+
+        var row=tempRow!!
+
+        return row
+
+
+    }
+
+}
