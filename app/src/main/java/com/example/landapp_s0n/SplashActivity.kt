e46ab8086f0cce4170e259617b8167b7a505d92f
s0n <shson971029@naver.com>
2022-08-16 16:07:11 +0900

    핸들러를 이용해서 2.5초 후에 메인으로 이동

--- app/src/main/java/com/example/landapp_s0n/SplashActivity.kt
+++ app/src/main/java/com/example/landapp_s0n/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.example.landapp_s0n
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed( {
+
+            //2.5초 후에 실행할 내용 작성
+//          메인 화면으로 이동 +지금 화면 종료
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        }, 2500 )
+
     }
 }
